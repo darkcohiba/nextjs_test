5d3d766c73de1ca7c2b786aea56de66a53c1d9bb
Sam <samuel.waters@gmail.com>
2022-10-31 09:51:05 -0600

    more

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import '../styles/globals.css'
 
+//next js fetch request
+export async function getServerSideProps(context) {
+  const res = await fetch('http://localhost:3000/api/hello')
+  const data = await res.json()
+
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    //json data for prisoner
+    prisoner: {
+      firstName: "John",
+      lastName: "Doe",
+      inmateNumber: "123456",
+      location: "San Quentin",
+      unit: "A",
+      cell: "1",
+      dateOfBirth: "01/01/1990",
+      name: 'John Doe',
+      age: 30,
+   }})
 }
